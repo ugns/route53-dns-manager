597cc1e47ee18878b162ad5a30bd609c8de25590
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-07-14 17:14:10 -0400

    fix: update GitHub Actions workflow to install Node.js dependencies directly and remove unnecessary null resource

--- terraform/lambda_build.tf
+++ terraform/lambda_build.tf
@@ -1,9 +1,3 @@
-resource "null_resource" "dns_manager_npm_install" {
-  provisioner "local-exec" {
-    command = "cd ${path.module}/../amplify/backend/function/dnsManager && npm install"
-  }
-}
-
 data "archive_file" "dns_manager_zip" {
   depends_on  = [null_resource.dns_manager_npm_install]
   type        = "zip"
