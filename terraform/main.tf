5f5723099dc00c0f49d7e7efa7948404603a9752
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-07-14 19:40:18 -0400

    fix: comment out unused custom rules in AWS Amplify app configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,29 +10,29 @@ resource "aws_amplify_app" "website" {
     REACT_APP_DNS_API_URL      = "${aws_api_gateway_stage.production.invoke_url}/api/dns"
   }
 
-  custom_rule {
-    source = "/static/<*>"
-    target = "/static/<*>"
-    status = "200"
-  }
+  # custom_rule {
+  #   source = "/static/<*>"
+  #   target = "/static/<*>"
+  #   status = "200"
+  # }
 
-  custom_rule {
-    source = "/favicon.ico"
-    target = "/favicon.ico"
-    status = "200"
-  }
+  # custom_rule {
+  #   source = "/favicon.ico"
+  #   target = "/favicon.ico"
+  #   status = "200"
+  # }
 
-  custom_rule {
-    source = "/manifest.json"
-    target = "/manifest.json"
-    status = "200"
-  }
+  # custom_rule {
+  #   source = "/manifest.json"
+  #   target = "/manifest.json"
+  #   status = "200"
+  # }
 
-  custom_rule {
-    source = "/<*>"
-    target = "/index.html"
-    status = "200"
-  }
+  # custom_rule {
+  #   source = "/<*>"
+  #   target = "/index.html"
+  #   status = "200"
+  # }
 }
 
 resource "aws_amplify_branch" "main" {
