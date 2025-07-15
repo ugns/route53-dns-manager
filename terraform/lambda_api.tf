cc49a4fe7b2bd2118fc3d64cce67ce49a2646a06
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-07-15 00:30:52 -0400

    fix: update API Gateway deployment to include lambda function dependency and improve lifecycle management

--- terraform/lambda_api.tf
+++ terraform/lambda_api.tf
@@ -109,13 +109,16 @@ resource "aws_lambda_permission" "allow_apigw" {
 resource "aws_api_gateway_deployment" "dns_manager_deployment" {
   depends_on = [
     aws_api_gateway_integration.dns_manager_integration,
-    aws_api_gateway_integration.dns_manager_options_integration
+    aws_api_gateway_integration.dns_manager_options_integration,
+    aws_lambda_function.dns_manager
   ]
   rest_api_id = aws_api_gateway_rest_api.dns_manager_api.id
   triggers = {
-    manager        = aws_api_gateway_integration.dns_manager_integration.resource_id
-    options        = aws_api_gateway_integration.dns_manager_options_integration.resource_id
-    lambda_version = aws_lambda_function.dns_manager.version
+    lambda_version = aws_lambda_function.dns_manager.qualified_invoke_arn
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
